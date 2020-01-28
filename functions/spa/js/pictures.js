ff0bc9d892bb8a96b20bee918bc56814cdd3285c
saitharunkumar10 <saitharunkumar10.com>
2020-01-27 21:47:49 -0600

    init

---
+++ functions/spa/js/pictures.js
@@ -0,0 +1,24 @@
+function pictures(){
+    pageContent.innerHTML = `
+
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <img src="http://timberlakeconstruction.com/images/projects/113/uco-sb-4717-010_resized-w2400h0.jpg" class="d-block w-90" alt="..." width="1400" height="700">
+    </div>
+    <div class="carousel-item">
+      <img src="https://media.uco.edu/press/uploads/2017/09/oldnorthatuco.jpg" class="d-block w-90" alt="..." width="1400" height="700">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+  </div>
+    
+    `;
+}
